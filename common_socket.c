c1a90d41f57cf35e951bc8e0a25b2b8e562509ed
fangrobinson <fang.robinson@gmail.com>
2020-10-20 12:25:57 -0300

    ADD code socket bind and listen

--- common_socket.c
+++ common_socket.c
@@ -14,6 +14,31 @@ int socket_bind_and_listen(socket_t *socket, unsigned short port){
 	char port_string[4];
 	sprintf(port_string, "%hu", port);
 	int status = getaddrinfo(0, port_string, &hints, &results);
+	if (status == 1) {
+		return status;
+	}
+
+	socket->socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+	if (socket->socket == -1) {
+		freeaddrinfo(results);
+		return 1;
+	}
+
+	int valid_bind = bind(socket->socket, results->ai_addr, results->ai_addrlen);
+	if (valid_bind != SUCCESS) {
+		freeaddrinfo(results);
+		socket_uninit(socket);
+		return 1;
+	}
+	
+	int valid_listen = listen(socket->socket, MAX_CONNECTIONS);
+	if (valid_listen != SUCCESS) { 
+		freeaddrinfo(results);
+		socket_uninit(socket);
+		return 1;
+	}
+
+
 	freeaddrinfo(results);
 	return SUCCESS;
 
