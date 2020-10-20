bad52601c5aa7c2f90ef093425907e4360e98cad
fangrobinson <fang.robinson@gmail.com>
2020-10-20 12:52:01 -0300

    Socket tda now compiles test pending

--- common_socket.c
+++ common_socket.c
@@ -1,6 +1,9 @@
+#define _XOPEN_SOURCE 600
+
 #include <string.h>
 #include <stdio.h>
 #include <netdb.h>
+
 #include <unistd.h>
 #include <sys/socket.h>
 #include "common_socket.h"
@@ -13,7 +16,7 @@ int socket_uninit(socket_t *socket){
 	return close(socket->socket);
 }
 
-int socket_bind_and_listen(socket_t *socket, unsigned short port){
+int socket_bind_and_listen(socket_t *a_socket, unsigned short port){
 	struct addrinfo hints;
 	struct addrinfo *results;
 
@@ -29,23 +32,23 @@ int socket_bind_and_listen(socket_t *socket, unsigned short port){
 		return status;
 	}
 
-	socket->socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
-	if (socket->socket == -1) {
+	a_socket->socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+	if (a_socket->socket == -1) {
 		freeaddrinfo(results);
 		return 1;
 	}
 
-	int valid_bind = bind(socket->socket, results->ai_addr, results->ai_addrlen);
+	int valid_bind = bind(a_socket->socket, results->ai_addr, results->ai_addrlen);
 	if (valid_bind != 0) {
 		freeaddrinfo(results);
-		socket_uninit(socket);
+		socket_uninit(a_socket);
 		return 1;
 	}
 	
-	int valid_listen = listen(socket->socket, 1);
+	int valid_listen = listen(a_socket->socket, 1);
 	if (valid_listen != 0) { 
 		freeaddrinfo(results);
-		socket_uninit(socket);
+		socket_uninit(a_socket);
 		return 1;
 	}
 
@@ -53,7 +56,7 @@ int socket_bind_and_listen(socket_t *socket, unsigned short port){
 	return 0;
 }
 
-int socket_connect(socket_t *socket, const char *server, unsigned short port){
+int socket_connect(socket_t *a_socket, const char *server, unsigned short port){
 
 	struct addrinfo hints;
 	struct addrinfo *results;
@@ -65,21 +68,21 @@ int socket_connect(socket_t *socket, const char *server, unsigned short port){
 	
 	char port_string[4];
 	sprintf(port_string, "%d", port);
-	int status = getaddrinfo(host_name, port_string, &hints, &results);
+	int status = getaddrinfo(server, port_string, &hints, &results);
 	if (status == 1) {
 		return 1;
 	}
 	
-	socket->socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
-	if (socket->socket == -1) {
+	a_socket->socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+	if (a_socket->socket == -1) {
 		freeaddrinfo(results);
 		return 1;
 	}
 
-	int valid_connect = connect(socket->socket, results->ai_addr, results->ai_addrlen);
+	int valid_connect = connect(a_socket->socket, results->ai_addr, results->ai_addrlen);
 	if (valid_connect == 1) {
 		freeaddrinfo(results);
-		socket_uninit(socket);
+		socket_uninit(a_socket);
 		return 1;
 	}
 	
@@ -107,7 +110,7 @@ int socket_send(socket_t *socket, const char *buffer, size_t length){
 	while (length >= bytes_sent && socket_open) {
 		int bytes_to_add = send(socket->socket, (buffer + bytes_sent), length - bytes_sent,
 		MSG_NOSIGNAL);
-		if (s > 0){
+		if (bytes_to_add > 0){
 			bytes_sent += bytes_to_add;
 		} else {
 			socket_open = 0;
